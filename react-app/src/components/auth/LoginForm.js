81515f3cc903555d2e7e093a952d865184f4c9b8
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-09-18 08:46:25 -0500

    finished merging

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
-import './auth.css'
 
 import './Auth.css'
 
